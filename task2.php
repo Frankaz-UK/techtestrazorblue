a6c2c9035acb9850473203be39fa8af82d9af0a0
Frankaz <frankazx@gmail.com>
2025-06-26 22:43:45 +0100

    Add SQL For Task 2 (Commented Out For Safety)

--- task2.php
+++ task2.php
@@ -1,5 +1,27 @@
 <?php
 // Task 2
+/*
+// Basic PDO connection, would not be used in Laravel as you'd go via the model instead!
+// Also dangerous to have connection information in a php file too, better to store in a .env
+$host = '127.0.0.1';
+$db   = 'test';
+$user = 'root';
+$pass = 'password';
+$port = '3306';
+$charset = 'utf8mb4';
+
+$options = [
+    \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+    \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+    \PDO::ATTR_EMULATE_PREPARES   => false,
+];
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset;port=$port";
+$pdo = new \PDO($dsn, $user, $pass, $options);
+
+$stmt = $pdo->query("SELECT ip_address FROM ip_address"); // see the SQL file task2.sql
+$ipList = $stmt->fetch();
+*/
+
 function isIpAllowed($ip, $ipList): bool
 {
     if (!filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
